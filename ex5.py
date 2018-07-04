5b95033d001ca94cf4a4ef12659349644be59aec
lohithkumaran <lohithkumar.shetty@gmail.com>
2018-07-04 12:10:50 +0530

    my ex5 file

---
+++ ex5.py
@@ -0,0 +1,16 @@
+#new line
+print("Mary had a little lamb.")
+print("And everywhere that Mary went.")
+print("."*10)
+end1="c"
+end2="h"
+end3="e"
+end4="e"
+end5="s"
+end6="e"
+end7="b"
+end8="u"
+end9="r"
+end10="g"
+end11="e"
+end12="r"
