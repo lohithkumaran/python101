1f3cd62bb2b8fb4409e45fcebb9b48b5e5a30b16
lohithkumaran <lohithkumar.shetty@gmail.com>
2018-07-05 17:39:48 +0530

    my hw2

---
+++ hw2.py
@@ -0,0 +1,11 @@
+a= float(input('enter a: '))
+b= float(input('enter b: '))
+c= float(input('enter c: '))
+if (a>b):
+	print("a is greater than b")
+else:
+	print("b is greater than a")
+if(c>a):
+	print("c is greater than a")
+else:
+	print("c is greater than b")
