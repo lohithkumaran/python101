224d7f894cda66c198a991576665fc3866e54de3
lohithkumaran <lohithkumar.shetty@gmail.com>
2018-07-05 22:08:58 +0530

    my hw3 file

---
+++ hw3.py
@@ -0,0 +1,75 @@
+
+1. isalnum() :
+
+print('str1.isalnum()=',str1.isalnum())
+str1.isalnum()= True
+checks if the string is numbers or alphabets and returns true else returns false
+
+
+2. isalpha() :
+
+print('str1.isalpha()=',str1.isalpha())
+str1.isalpha()= True
+checks if the string is alphabets and returns true else returns false
+
+
+
+3. isdecimal() :
+
+print('str1.isdecimal()=',str1.isdecimal())
+str1.isdecimal()= False
+checks if the string is numbers and returns true else returns false
+
+
+
+4. isdigit() :
+
+print('str1.isdigit()=',str1.isdigit())
+str1.isdigit()= False
+checks if the string is numbers and returns true else returns false
+
+
+5 .upper():
+
+str='lohith'
+ print('str.upper()=',str.upper())
+str.upper()= LOHITH
+It converts from lower case to upper case
+
+6. lower():
+str='LOHITH'
+
+print('str.lower()=',str.lower())
+str.lower()= lohith
+It converts from upper case to lower case
+
+
+6. enter() :
+
+print('str1.center(1)=',str1.center(1))
+str1.center(1)= btm
+print('str1.center(2)=',str1.center(2))
+str1.center(2)= btm
+
+7. encode() :
+
+print('str1.encode()=',str1.encode())
+str1.encode()= b'btm'
+
+
+8. find():
+ 
+print('str1.find()=',str1.find())
+TypeError: find() takes at least 1 argument (0 given)
+
+9. isspace() 
+str=' '
+
+ print('str.isspace()=',str.isspace())
+str.isspace()= True
+returns true if the string is empty else false
+
+
+ 
+
+
