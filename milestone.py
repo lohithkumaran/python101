046dac3dc212ee6a5eb2059ac04327e4d133c5a3
lohithkumaran <lohithkumar.shetty@gmail.com>
2018-07-08 22:58:55 +0530

    my milestone file

---
+++ milestone.py
@@ -0,0 +1,57 @@
+Palindrome : 
+
+def swap(i,j):
+    n=5
+    word=input("enter a word")
+    i=0
+    j=n
+    temp=0
+    for i in word:
+        for j in word:
+            if (i==j):
+                swap(i,j)
+                temp=i
+                i=j
+                j=temp
+    i=i+1
+    j=j-1
+
+
+vowels:
+
+Vowel=[‘a’,’e’,’i’,’o’,’u’]
+word=input ("enter a word")
+count=0
+for letter in word:
+    if letter in vowel:
+    	count=count+1               
+print(count)
+
+Bmi Calculator:
+
+height = float(input("Your height in metres:"))
+weight = int(input("Your weight in kilograms:"))
+bmi = round(weight/ (height * height), 1)
+
+if bmi <= 18.5:
+     print('Your BMI is', bmi, 'which means you are underweight.')
+
+elif bmi > 18.5 and bmi < 25:
+     print('Your BMI is', bmi, 'which means you are normal.')
+
+elif bmi > 25 and bmi < 30:
+     print('Your BMI is', bmi, 'which means you are overweight.')
+
+elif bmi > 30:
+     print('Your BMI is', bmi, 'which means you are obese.')
+
+else:
+    print('There is an error with your input')
+
+Pattren:  
+
+for i in range(0,n):
+for j in range(0,i+1):
+       	print("  *  ",end="")
+       print("\r")
+   
