03c50a37424f0c36884bd63af1f3a4e3ef77f2c5
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-19 22:56:41 +0530

    Update milestone.py

--- milestone.py
+++ milestone.py
@@ -9,30 +9,31 @@ for letter in word:
 print(count)
 
 Bmi Calculator:
+def bmi(height,weight):
+    height = float(input("Your height in metres:"))
+    weight = int(input("Your weight in kilograms:"))
+    bmi = round(weight/ (height * height), 1)
 
-height = float(input("Your height in metres:"))
-weight = int(input("Your weight in kilograms:"))
-bmi = round(weight/ (height * height), 1)
+    if bmi <= 18.5:
+        print('Your BMI is', bmi, 'which means you are underweight.')
 
-if bmi <= 18.5:
-     print('Your BMI is', bmi, 'which means you are underweight.')
+    elif bmi > 18.5 and bmi < 25:
+        print('Your BMI is', bmi, 'which means you are normal.')
 
-elif bmi > 18.5 and bmi < 25:
-     print('Your BMI is', bmi, 'which means you are normal.')
+    elif bmi > 25 and bmi < 30:
+        print('Your BMI is', bmi, 'which means you are overweight.')
 
-elif bmi > 25 and bmi < 30:
-     print('Your BMI is', bmi, 'which means you are overweight.')
+    elif bmi > 30:
+        print('Your BMI is', bmi, 'which means you are obese.')
 
-elif bmi > 30:
-     print('Your BMI is', bmi, 'which means you are obese.')
+    else:
+        print('There is an error with your input')
+bmi()
+  
+def pattern(n):
+    for i in range(0,n):
+        for j in range(0,i+1):
+       	    print("  *  ",end="")
+            print("\r")
+ pattern()
 
-else:
-    print('There is an error with your input')
-
-Pattren:  
-
-for i in range(0,n):
-for j in range(0,i+1):
-       	print("  *  ",end="")
-       print("\r")
-   
