e97216f9d20498faf8b2de1335e626fafa88aedb
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 19:05:36 +0530

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -7,3 +7,14 @@ res1 =(-b-cmath.sqrt(d))/(2*a)
 res2 =(-b+cmath.sqrt(d))/(2*a)
 print('the solution are {0} and {1}'.format(res1,res2))
 
+output
+1.
+enter a: 5
+enter b: 2
+enter c: 6
+the solution are (-0.2-1.0770329614269007j) and (-0.2+1.0770329614269007j)
+2.
+enter a: 10
+enter b: 20
+enter c: 30
+the solution are (-1-1.4142135623730951j) and (-1+1.4142135623730951j)
