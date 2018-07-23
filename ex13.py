257fbafad96941ecf0aa65ee669da39918b0dc0c
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 19:10:45 +0530

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -8,3 +8,13 @@ if sys.argv < 2:
 
 filename = sys.argv[1]
 print('Filename:', filename)
+
+output
+$ python 1.py
+Arguments: 1
+List: ['1.py']
+
+ output
+$ python 1.py lohith
+Arguments: 2
+List: ['1.py', 'lohith']
