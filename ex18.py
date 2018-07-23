4a7b1fdab005977a1ddc76b0a678ffd5288f7bae
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 19:29:40 +0530

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -1,13 +1,8 @@
-# Python program to check if the input number is prime or not
 
 num = 407
 
-# take input from the user
-# num = int(input("Enter a number: "))
-
-# prime numbers are greater than 1
 if num > 1:
-   # check for factors
+   
    for i in range(2,num):
        if (num % i) == 0:
            print(num,"is not a prime number")
@@ -16,7 +11,15 @@ if num > 1:
    else:
        print(num,"is a prime number")
        
-# if input number is less than
-# or equal to 1, it is not prime
+
 else:
    print(num,"is not a prime number")
+   
+output
+67 is a prime number
+
+output
+6 is not a prime number
+2 times 3 is 6
+
+   
