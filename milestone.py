c65251899c1b3c610b0f821d3144ac80fdbe5145
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 10:31:29 +0530

    Update milestone.py

--- milestone.py
+++ milestone.py
@@ -44,3 +44,29 @@ def reverse(s):
 s = input("enter a word")
 print(reverse(s))
 
+Special_character:
+def special_character(str):
+    special_character=['{','}','[',']','$','@','(',')','!']
+    word=input ("enter a word")
+    count=0
+    for letter in word:
+        if letter in special_character:
+            print(letter)
+            count=count+1
+    print(count)
+special_character()
+
+Factorial :
+def fact(n):
+    n = 5
+    fact = 1
+    for i in range(1,n+1):
+        fact = fact * i
+        print(fact)
+fact()        
+
+sentence=input("enter a words:")
+word=sentence.split()
+word.sort()
+for i in word:
+    print(i)
