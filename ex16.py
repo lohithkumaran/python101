2626a63db89bae9ae60eee55cad4cb3f8478747e
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 19:15:22 +0530

    Update ex16.py

--- ex16.py
+++ ex16.py
@@ -1,12 +1,8 @@
-# List of numbers
 numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
-
-# variable to store the sum
 sum = 0
-
-# iterate over the list
 for val in numbers:
 	sum = sum+val
+print("The sum is", sum)
 
-# Output: The sum is x
-print("The sum is", sum
+output 
+The sum is 48
