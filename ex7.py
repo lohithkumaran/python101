0b654db80273d3f69bdfc66cc326ff0f8b3ce7e1
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 18:46:51 +0530

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -3,3 +3,14 @@ months="Jan\nfeb\nmar\napr\nmay\njun\njul\naug"
 print("Here are the days:",days)
 print("Here are the months:",months)
 print("" "")
+
+output
+Here are the days: Mon Tue Wed Thu Fri Sat Sun
+Here are the months: Jan
+feb
+mar
+apr
+may
+jun
+jul
+aug
