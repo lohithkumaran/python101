1da517ea978e2bb876b69b6d8980da4892ebec12
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 19:13:46 +0530

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -21,3 +21,8 @@ if people > trucks:
     print("Alright, let's just take the trucks.")
 else:
     print("Fine, let's stay home then.")
+
+Output    
+We should take the cars.
+Maybe we could take the trucks.
+Alright, let's just take the trucks.
