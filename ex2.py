11d5e7f3c18ca57ab07a3904ed14733d2be88963
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 18:29:33 +0530

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,12 +1,12 @@
-^[[200~# A comment, this is so you can read your program later.
-        # Anything after the # is ignored by python.
+# A comment, this is so you can read your program late
+# Anything after the # is ignored by python.
 
-        print("I could have code like this.") # and the comment after is ignored
+print("I could have code like this.") # and the comment after is ignored
 
-        # You can also use a comment to "disable" or comment out a piece of code:
-        # print("This won't run.")
+# You can also use a comment to "disable" or comment out a piece of code:
+# print("This won't run.")
 
-        print("This will run.")
-~
-~
+print("This will run.")
 
+I could hacve code like this
+This will run
