09986ddcbdea544765c3fddab3f709720af352c1
lohithkumaran <40756006+lohithkumaran@users.noreply.github.com>
2018-07-23 18:27:12 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,8 +1,12 @@
-#good first program
 print("Hello World!")
+Hello World
 print("Hello Again")
+Hello Again
 print("I like typing this.")
+I Like tyoing this.
 print("This is fun.")
+This is fun.
 print('Yay! Printing.')
+Yay! Printing.
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.'
